aede483a89dd082573405ac93a1360ff368a6d75
emmanouile <manos.emmanouil@deliveryblueprints.com>
2019-10-16 08:46:04 +0100

    add context consumer to button class

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,12 +3,16 @@ import LanguageContext from "../contexts/LanguageContext";
 
 
 class Button extends React.Component{
-    static contextType = LanguageContext;
     render() {
-        const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
 
         return(
-            <button className={"ui button primary"}>{ text }</button>
+            <button className={"ui button primary"}>
+                <LanguageContext.Consumer>
+                    {(value)=> (
+                        value === 'english' ? 'Submit' : 'Voorleggen'
+                    )}}
+                </LanguageContext.Consumer>
+            </button>
         )
     }
 }
